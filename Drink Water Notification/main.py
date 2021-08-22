b12ee1e5e0f504101815b3166fa093eb11b1d868
Samyak-Bumb <88429776+Samyak-Bumb@users.noreply.github.com>
2021-08-22 16:05:27 +0530

    Add files via upload

---
+++ Drink Water Notification/main.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+if __name__ == "__main__":
+    notification.notify(
+        title = "Samyak Drink Water Now!",
+        message = "Time to Drink Water",
+        app_icon = "bg.ico",
+        timeout=10
+    )
