ed4c5d2828ba8aad787207a5b80fad70143a7fb0
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-10-04 18:46:23 +0530

    Update main.py

--- Drink Water Notification/main.py
+++ Drink Water Notification/main.py
@@ -1,10 +1,16 @@
 # Created By Samyak Bumb
+
 import time
 from plyer import notification
+
 if __name__ == "__main__":
+
     notification.notify(
         title = "Samyak Drink Water Now!",
+
         message = "Time to Drink Water",
+
         app_icon = "bg.ico",
+
         timeout=10
 )
