4bf2ec41948cc744840290d7b8386aae8e598445
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-10-14 17:25:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import time
 from plyer import notification
 
 if __name__ == "__main__":
-    notification.notify(
+
+    notification.notify
+(
         title = "Samyak Drink Water Now!",
         message = "Time to Drink Water",
         app_icon = "bg.ico",
+
         timeout=10
 )
